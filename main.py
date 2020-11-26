b0279c9e421a13bc687fe9faefca32e65b873dbe
Chantelle Chan <chantellechan1@alumni.ubc.ca>
2020-11-25 20:52:21 -0800

    changed query, added pagination functionality, started tweet processing

--- main.py
+++ main.py
@@ -20,8 +20,11 @@ import secrets
 def auth():
     return secrets.BEARER_TOKEN
 
-def create_url():
-    query = "cat has:images -is:retweet"
+def create_url(next_token=None):
+    # TODO: add next_token functionality to get multiple pages of results
+    # IDEA: if number of results less than max results, stop looping for results
+
+    query = "(canada OR british columbia OR vancouver OR toronto OR GTA OR ontario) (covid OR COVID-19 OR coronavirus) business -is:retweet"
     # Tweet fields are adjustable.
     # Options include:
     # attachments, author_id, context_annotations,
@@ -29,10 +32,18 @@ def create_url():
     # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
     # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
     # source, text, and withheld
-    tweet_fields = "tweet.fields=author_id"
-    url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}".format(
-        query, tweet_fields
-    )
+    tweet_fields = "tweet.fields=author_id,created_at,entities"
+    max_results = "max_results=11"
+
+    if next_token is None:
+        url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}&{}".format(
+            query, tweet_fields, max_results
+        )
+    else:
+        next = "next_token={}".format(next_token)
+        url = "https://api.twitter.com/2/tweets/search/recent?query={}&{}&{}&{}".format(
+            query, tweet_fields, max_results, next
+        )
     return url
 
 
@@ -48,14 +59,35 @@ def connect_to_endpoint(url, headers):
         raise Exception(response.status_code, response.text)
     return response.json()
 
+def process_tweets(tweet_json):
+    tweets = tweet_json['data']
+    meta_data = tweet_json['meta']
+
+    for i, tweet in enumerate(tweets):
+        print("{}: {}".format(i, tweet['text']))
+    
+    # TODO: do something to process these tweets and upload them to be saved somewhere
+
 
 def main():
     bearer_token = auth()
-    url = create_url()
-    headers = create_headers(bearer_token)
-    json_response = connect_to_endpoint(url, headers)
-    print(json.dumps(json_response, indent=4, sort_keys=True))
+    next_token = None
+    tweet_list = []
 
+    for i in range(2):
+        print('run #{}'.format(i))
+
+        if next_token is None:
+            url = create_url()
+        else:
+            url = create_url(next_token=next_token)
+        headers = create_headers(bearer_token)
+        json_response = connect_to_endpoint(url, headers)
+        # print(json.dumps(json_response, indent=4, sort_keys=True))
+
+        # pass dictionary of tweets to processing function
+        process_tweets(json_response)
+        next_token = json_response['meta']['next_token']
 
 if __name__ == "__main__":
     main()
