d62aa1c32d847252fd52fc8fd494a72606713dd7
Chantelle Chan <chantellechan1@alumni.ubc.ca>
2020-12-29 13:23:54 -0800

    changed port to be set by process environment variable

--- api/README.md
+++ api/README.md
@@ -4,5 +4,6 @@
 1. create heroku remote: `heroku create <heroku api app name>`
 2. set heroku remote: `heroku git:remote -a <heroku api app name>`
 3. push `/api` subdirectory to heroku: `git subtree push --prefix api heroku main`
+    - make sure changes are have been commited in git first
 4. make sure one instance is running: `heroku ps:scale web=1`
 5. 
--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 80 //8080 for testing, 80 for deployment
+const port = process.env.PORT || 5000 // 5000 for testing
 const routes = require('./routes')
 
 
 app.use(routes)
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`App listening on ${port}`)
 })
